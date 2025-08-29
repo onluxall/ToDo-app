9c93a01db7b3f49185b521cebfee2de5e9fad35c
Luxall <onluxall@gmail.com>
2025-08-29 13:18:46 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const TodoListScreen(),
+    );
+  }
+}
